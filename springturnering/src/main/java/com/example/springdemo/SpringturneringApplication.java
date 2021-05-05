89e67e5b19fb902b348a525dd14dd77a1960c2fb
JacobIngvarsson <77965223+JacobIngvarsson@users.noreply.github.com>
2021-05-05 14:32:48 +0200

    init spring api project

---
+++ springturnering/src/main/java/com/example/springdemo/SpringturneringApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringturneringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringturneringApplication.class, args);
+	}
+
+}
