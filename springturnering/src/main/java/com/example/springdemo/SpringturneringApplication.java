681ace51bb7a76a834d60d480d77de07633c26c9
JacobIngvarsson <77965223+JacobIngvarsson@users.noreply.github.com>
2021-05-28 14:36:30 +0200

    add swagger (#6)

--- springturnering/src/main/java/com/example/springdemo/SpringturneringApplication.java
+++ springturnering/src/main/java/com/example/springdemo/SpringturneringApplication.java
@@ -2,9 +2,20 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.Collections;
+
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan({"com.example.controller", "com.example.services", "com.example.repositories"})
 public class SpringturneringApplication {
 
@@ -12,4 +23,32 @@ public class SpringturneringApplication {
 		SpringApplication.run(SpringturneringApplication.class, args);
 	}
 
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		// Returns a prepared Docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(regex("/api.*"))
+				.apis(RequestHandlerSelectors.basePackage("com.example"))
+				.build()
+				.apiInfo(apiDetails());
+		
+	}
+	
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Players Api",
+				"Tournament tracking API",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact(
+						"Jacob Ingvarsson", 
+						"https://jacobingvarsson.com", 
+						"jacob.ingvarsson@gmail.com"),
+				"API License",
+				"https://jacob.ingvarsson.com",
+				Collections.emptyList());
+	}
 }
